8402913cd7afb737510ee32c835fd63c0588f6cb
Michaeljon Miller <michaeljon.miller@outlook.com>
2022-06-09 13:19:24 -0700

    more feature parser staging

--- feature-builder/feature-builder.py
+++ feature-builder/feature-builder.py
@@ -1,16 +1,26 @@
 from curses.ascii import isupper
 import re
 import json
+from urllib import parse
 
 #
 # list of primary regions in fasta
 # grep -e '^>[^ ]* Homo sapiens chromosome [1-9XY]*, GRCh38.p14 Primary Assembly' GCF_000001405.40_GRCh38.p14_genomic.fna
 
 # sanity-check regions in fasta
-# / awk '$1~/>/{a[$1]++}END{for (e in a) { if (a[e] > 1) { print e, a[e] }}}' GCF_000001405.40_GRCh38.p14_genomic.fna
+# awk '$1~/>/{a[$1]++}END{for (e in a) { if (a[e] > 1) { print e, a[e] }}}' GCF_000001405.40_GRCh38.p14_genomic.fna
 
 # count of features per region
 # awk '!($1~/#/){a[$1]++}END{for (e in a) { print e, a[e] }}' GCF_000001405.40_GRCh38.p14_genomic.gff  | sort -k2 -g -r
+
+
+# RefSeq sequences with the prefixes NC_, NT_, NW_,NG_, NM_, NR_ or NP_
+#   chromosome - NC_000023.11
+#   genomic contigs or scaffolds - NT_010718.17, NW_003315950.2
+#   gene/genomic region - NG_012232.1
+#   coding transcript - NM_004006.2
+#   non-coding transcript - NR_004430.2
+#   protein - NP_003997.1
 
 # index int8range layout
 
@@ -28,14 +38,25 @@ import json
 FASTA = "/Users/michaeljon/Downloads/grch38p14 genome_assemblies_genome_gff/ncbi-genomes-2022-06-06/GCF_000001405.40_GRCh38.p14_genomic.fna"
 GFF = "/Users/michaeljon/Downloads/grch38p14 genome_assemblies_genome_gff/ncbi-genomes-2022-06-06/GCF_000001405.40_GRCh38.p14_genomic.gff"
 
+REFERENCE = "GRCh38.p14"
+ACCESSION = "NCBI_Assembly:GCF_000001405.40"
 
-ISPRIMARY_MASK =   0x8000000000000000
-RESERVED_MASK =    0x7F00000000000000
-CHROMOSOME_MASK =  0x00FF000000000000
-SEQUENCE_MASK =    0x0000FFFF00000000
-LOCUS_MASK =       0x00000000FFFFFFFF
+ISPRIMARY_MASK = 0x8000000000000000
+RESERVED_MASK = 0x7F00000000000000
+CHROMOSOME_MASK = 0x00FF000000000000
+SEQUENCE_MASK = 0x0000FFFF00000000
+LOCUS_MASK = 0x00000000FFFFFFFF
 
 sequences = {}
+sequenceTypes = {
+    "NC": "chromosome",
+    "NT": "genomic contig or scaffold",
+    "NW": "genomic contig or scaffold",
+    "NG": "gene/genomic region",
+    "NM": "coding transcript",
+    "NR": "non-coding transcript",
+    "NP": "protein",
+}
 
 
 def processFeature(line, lineNumber):
@@ -44,15 +65,20 @@ def processFeature(line, lineNumber):
 
     # if we've been asked to process a feature where there's no
     # matching sequence in the FASTA, we have no choice but to bail
-    if ("sequenceId" in sequences[columns[0]]) == False:
+    if ("sequenceTypeId" in sequences[columns[0]]) == False:
         return
 
     feature["featureLine"] = lineNumber
+    feature["sequenceTypeId"] = sequences[columns[0]]["sequenceTypeId"]
+
+    feature["reference"] = REFERENCE
+    feature["assembly"] = ACCESSION
     feature["sequenceName"] = columns[0]
-    feature["sequenceId"] = sequences[columns[0]]["sequenceId"]
-    feature["type"] = columns[2]
+
+    feature["featureType"] = columns[2]
     feature["startLocus"] = int(columns[3])
     feature["endLocus"] = int(columns[4])
+    feature["info"] = {}
 
     infos = columns[8].split(";")
     if infos != None and len(infos) > 0:
@@ -63,10 +89,17 @@ def processFeature(line, lineNumber):
             elif i.startswith("gene="):
                 feature["Gene"] = v[1]
             elif i.startswith("Dbxref="):
-                identities = v[1].split(',')
-                feature["alternateIdentities"] = { k:v for k,v in zip([k.split(':')[0] for k in identities],[':'.join(k.split(':')[1:]) for k in identities]) }
+                identities = v[1].split(",")
+                feature["alternateIdentities"] = {
+                    k: v
+                    for k, v in zip(
+                        [k.split(":")[0] for k in identities], [":".join(k.split(":")[1:]) for k in identities]
+                    )
+                }
             elif isupper(i[0][0]):
                 feature[v[0]] = v[1]
+            else:
+                feature["info"][v[0].lower()] = parse.unquote(v[1])
 
     chromosome = sequences[columns[0]]["chromosome"]
     startKey = 0x0000000000000000
@@ -85,7 +118,7 @@ def processFeature(line, lineNumber):
     else:
         startKey |= int(chromosome) << 48
 
-    startKey |= feature["sequenceId"] << 32
+    startKey |= feature["sequenceTypeId"] << 32
     if sequences[columns[0]]["isPrimary"] == True:
         startKey |= ISPRIMARY_MASK
 
@@ -98,11 +131,11 @@ def processFeature(line, lineNumber):
 
     feature["chromosome"] = sequences[columns[0]]["chromosomeName"]
 
-    feature["key"] = { "start": startKey, "end": endKey}
-    feature["formattedKey"] = { "start": "0x" + format(startKey, "016x"), "end": "0x" + format(endKey, "016x")}
+    feature["key"] = {"start": startKey, "end": endKey}
+    feature["formattedKey"] = {"start": "0x" + format(startKey, "016x"), "end": "0x" + format(endKey, "016x")}
 
-    # print(json.dumps(feature, indent=2,sort_keys=False))
-    print(feature)
+    print(json.dumps(feature, indent=2, sort_keys=False))
+    # print(feature)
 
 
 def processFeatures():
@@ -120,62 +153,45 @@ def processFeatures():
 
 
 def processSequence(line, sequenceId):
-    rex = r">(?P<sequenceName>[^ ]*) Homo sapiens (?P<flavor>(chromosome|unplaced.*|mitochondrion.*)) (?P<chromosome>[0-9XY]*)"
+    rex = r">(?P<sequenceType>(NC|NT|NW|NG|NM|NR|NP))_(?P<sequenceName>[^ ]*) Homo sapiens (?P<flavor>(chromosome|unplaced.*|mitochondrion.*)) (?P<chromosome>[0-9XY]*)"
     sMatch = re.search(rex, line)
 
     if sMatch == None:
         return
 
+    sequenceType = sMatch.group("sequenceType")
     sequenceName = sMatch.group("sequenceName")
     chromosome = sMatch.group("chromosome")
-    flavor = sMatch.group("flavor").split(' ')[0]
+    flavor = sMatch.group("flavor").split(" ")[0]
     description = line[1:]
+
+    sequence = {
+        "sequenceTypeCode": sequenceType,
+        "sequenceType": sequenceTypes[sequenceType],
+        "sequenceTypeId": sequenceId,
+        "sequenceName": sequenceType + "_" + sequenceName,
+        "description": description,
+        "isPrimary": False,
+    }
 
     match flavor:
         case "unplaced":
-            sequence = {
-                "sequenceId": sequenceId,
-                "sequenceName": sequenceName,
-                "isPrimary": False,
-                "chromosomeName": "unplaced",
-                "chromosome": "Z",
-                "description": description,
-            }
+            sequence["chromosomeName"] = "unplaced"
+            sequence["chromosome"] = "Z"
 
         case "chromosome":
-            if line.endswith(", GRCh38.p14 Primary Assembly"):
-                sequence = {
-                    "sequenceId": sequenceId,
-                    "sequenceName": sequenceName,
-                    "isPrimary": True,
-                    "chromosomeName": "chr" + chromosome,
-                    "chromosome": chromosome,
-                    "description": description,
-                }
-            else:
-                sequence = {
-                    "sequenceId": sequenceId,
-                    "sequenceName": sequenceName,
-                    "isPrimary": False,
-                    "chromosomeName": "chr" + chromosome,
-                    "chromosome": chromosome,
-                    "description": description,
-                }
+            sequence["chromosomeName"] = "chr" + chromosome
+            sequence["chromosome"] = chromosome
+            sequence["isPrimary"] = sequenceType == "NC"
 
         case "mitochondrion,":
-            sequence = {
-                "sequenceId": sequenceId,
-                "sequenceName": sequenceName,
-                "isPrimary": False,
-                "chromosomeName": "chrM",
-                "chromosome": "M",
-                "description": description,
-            }
+            sequence["chromosomeName"] = "chrM"
+            sequence["chromosome"] = "M"
 
         case _:
             sequence = {}
 
-    sequences[sequenceName] = sequence
+    sequences[sequenceType + "_" + sequenceName] = sequence
 
 
 def processSequences():
@@ -193,4 +209,5 @@ def processSequences():
 
 
 processSequences()
+# print(json.dumps(sequences, indent=2,sort_keys=False))
 processFeatures()
