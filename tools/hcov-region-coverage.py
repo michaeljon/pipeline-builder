854204eab170e0b18e5145a35c52a9400a84bcbf
Michaeljon Miller <michaeljon.miller@outlook.com>
2022-09-02 11:34:44 -0700

    adding org-wide stats

--- tools/hcov-region-coverage.py
+++ tools/hcov-region-coverage.py
@@ -27,13 +27,10 @@ organism_sequences = {k: v["sequence"] for _, (k, v) in enumerate(feature_data.i
 
 for ok in feature_data:
     org = feature_data[ok]
+    org["region_data"] = [0 for _ in range(org["region"]["start"], org["region"]["stop"] + 1)]
     for gk in org["genes"]:
         gene = org["genes"][gk]
         gene["region_data"] = [0 for _ in range(gene["start"], gene["stop"] + 1)]
-
-# sequence_id = feature_data[organism]["sequence"]
-# genes = feature_data[organism]["genes"]
-# region_data = {k: [0 for _ in range(v["start"], v["stop"] + 1)] for i, (k, v) in enumerate(genes.items())}
 
 with gzip.open(pileup, "rt") as f:
     while line := f.readline().rstrip():
@@ -42,11 +39,16 @@ with gzip.open(pileup, "rt") as f:
 
             if organism == "*" or organism == sequence_organisms[elements[0]]:
                 pos = int(elements[1])
+
+                target = feature_data[sequence_organisms[elements[0]]]
+
                 infos = elements[7].split(";")
                 for i in infos:
                     v = i.split("=")
-                    if i.startswith("DP="):
-                        genes = feature_data[sequence_organisms[elements[0]]]["genes"]
+                    if v[0] == "DP":
+                        target["region_data"][pos - 1] = int(v[1])
+
+                        genes = target["genes"]
                         for g in genes:
                             gene = genes[g]
                             if genes[g]["start"] <= pos and pos <= genes[g]["stop"]:
@@ -55,6 +57,32 @@ with gzip.open(pileup, "rt") as f:
 print("sample\torganism\tgene\tmeandepth\tmediandepth\tmindepth\tmaxdepth\tstdev\tbases\tseen\tcoverage")
 for ok in feature_data:
     if organism == "*" or organism == ok:
+        region_data = feature_data[ok]["region_data"]
+
+        mean = np.mean(region_data)
+        median = np.median(region_data)
+        min = np.min(region_data)
+        max = np.max(region_data)
+        stdev = np.std(region_data)
+        read_count = sum([1 for dp in region_data if dp != 0])
+        width = feature_data[ok]["region"]["stop"] - feature_data[ok]["region"]["start"] + 1
+
+        print(
+            "{}\t{}\t{}\t{:.1f}\t{:.0f}\t{:.0f}\t{:.0f}\t{:.2f}\t{:.0f}\t{:.0f}\t{:.2f}".format(
+                sample,
+                ok,
+                "*",
+                mean,
+                median,
+                min,
+                max,
+                stdev,
+                width,
+                read_count,
+                read_count / width * 100.0,
+            )
+        )
+
         genes = feature_data[ok]["genes"]
         for gk in genes:
             region_data = genes[gk]["region_data"]
