72a9586fd38d5ac881cb17e0dfcaa650e0f2bfb3
Michaeljon Miller <michaeljon.miller@outlook.com>
2022-11-07 20:49:28 +0000

    changing intervals to primary only

--- human/build-hg38-pipeline.py
+++ human/build-hg38-pipeline.py
@@ -609,12 +609,12 @@ def alignAndSort(script: TextIOWrapper, options: OptionsDict, output: str):
 # Write intervals file computed from aligned and sorted BAM
 samtools view --header-only {BAM} | \\
     grep --color=never '^@SQ' | \\
-    python {BIN}/interval-builder.py --segment {SEGMENT} --factor {FACTOR} --process intervalList > {PIPELINE}/{SAMPLE}.intervals.tsv
+    python {BIN}/interval-builder.py --primaryOnly --segment {SEGMENT} --factor {FACTOR} --process intervalList > {PIPELINE}/{SAMPLE}.intervals.tsv
 
 # Write VCF merge list file computed from aligned and sorted BAM
 samtools view --header-only {BAM} | \\
     grep --color=never '^@SQ' | \\
-    python {BIN}/interval-builder.py --segment {SEGMENT} --factor {FACTOR} --process mergeList --root {PIPELINE}/{SAMPLE} > {PIPELINE}/{SAMPLE}.merge.list
+    python {BIN}/interval-builder.py --primaryOnly --segment {SEGMENT} --factor {FACTOR} --process mergeList --root {PIPELINE}/{SAMPLE} > {PIPELINE}/{SAMPLE}.merge.list
 """.format(
             BIN=bin, BAM=output, PIPELINE=pipeline, SAMPLE=sample, SEGMENT=segmentSize, FACTOR=factor
         )
