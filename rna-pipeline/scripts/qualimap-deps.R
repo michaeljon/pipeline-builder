49c929631a43845f5e067e050d1518794ecc65c9
Michaeljon Miller <michaeljon.miller@outlook.com>
2023-03-31 02:02:35 +0000

    working rna pipeline

--- rna-pipeline/scripts/qualimap-deps.R
+++ rna-pipeline/scripts/qualimap-deps.R
@@ -1,31 +1,12 @@
 # CountsQC
 
-if (!require("optparse")) {
-    install.packages("optparse", repos = "http://cran.r-project.org")
-}
-
-if (!require("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
-}
-
-if (!require("NOISeq")) {
-    BiocManager::install("NOISeq")
-}
+if (!require("optparse")) install.packages("optparse", repos = "http://cran.r-project.org")
+if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+if (!require("NOISeq")) BiocManager::install("NOISeq")
 
 # Epigenetics
 
-if (!require("XML")) {
-    install.packages("XML", repos = "http://cran.r-project.org")
-}
-
-if (!require("Repitools")) {
-    BiocManager::install("Repitools")
-}
-
-if (!require("Rsamtools")) {
-    BiocManager::install("Rsamtools")
-}
-
-if (!require("rtracklayer")) {
-    BiocManager::install("rtracklayer")
-}
+if (!require("XML")) install.packages("XML", repos = "http://cran.r-project.org")
+if (!require("Repitools")) BiocManager::install("Repitools")
+if (!require("Rsamtools")) BiocManager::install("Rsamtools")
+if (!require("rtracklayer")) BiocManager::install("rtracklayer")
